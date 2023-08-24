25e804e3637afb7d41830739baf1642188929362
Jenit Harnesha <104550437+JenitJain@users.noreply.github.com>
2023-08-24 14:48:54 +0530

    Update DAA_Lab_5_1.c

--- LAB5/DAA_Lab_5_1.c
+++ LAB5/DAA_Lab_5_1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct person {
   int id;
@@ -9,121 +10,62 @@ struct person {
   int weight;
 };
 
-void read_data(struct person *array, int n) {
-  // Open the file.
-  FILE *fp = fopen("data.txt", "r");
-  if (fp == NULL) {
-    printf("Error opening file.\n");
-    exit(1);
-  }
-
-  // Read the data from the file.
+void read_data(struct person *people, int n) {
   for (int i = 0; i < n; i++) {
-    fscanf(fp, "%d %s %d %d %d", &array[i].id, array[i].name, &array[i].age, &array[i].height, &array[i].weight);
+    printf("Id: ");
+    scanf("%d", &people[i].id);
+    printf("Name: ");
+    scanf("%s", people[i].name);
+    printf("Age: ");
+    scanf("%d", &people[i].age);
+    printf("Height: ");
+    scanf("%d", &people[i].height);
+    printf("Weight(pound): ");
+    scanf("%d", &people[i].weight);
   }
-
-  // Close the file.
-  fclose(fp);
 }
 
-void create_min_heap(struct person *array, int n) {
-  // Initialize the heap.
+void min_heapify(struct person *people, int i, int n) {
+  int smallest = i;
+  int left = 2 * i + 1;
+  int right = 2 * i + 2;
+
+  if (left < n && people[left].age < people[smallest].age) {
+    smallest = left;
+  }
+
+  if (right < n && people[right].age < people[smallest].age) {
+    smallest = right;
+  }
+
+  if (smallest != i) {
+    struct person temp = people[i];
+    people[i] = people[smallest];
+    people[smallest] = temp;
+
+    min_heapify(people, smallest, n);
+  }
+}
+
+void build_min_heap(struct person *people, int n) {
   for (int i = n / 2 - 1; i >= 0; i--) {
-    min_heapify(array, i, n);
-  }
-}
-
-void create_max_heap(struct person *array, int n) {
-  // Initialize the heap.
-  for (int i = n / 2 - 1; i >= 0; i--) {
-    max_heapify(array, i, n);
-  }
-}
-
-void display_weight_of_youngest_person(struct person *array, int n) {
-  // Find the minimum weight.
-  int min_weight = array[0].weight;
-  int min_index = 0;
-  for (int i = 1; i < n; i++) {
-    if (array[i].weight < min_weight) {
-      min_weight = array[i].weight;
-      min_index = i;
-    }
-  }
-
-  // Print the weight of the youngest person.
-  printf("Weight of youngest student: %.2f kg\n", min_weight * 0.45359237);
-}
-
-void insert_into_min_heap(struct person *array, int *n, struct person person) {
-  // Increase the size of the heap.
-  (*n)++;
-
-  // Insert the person into the heap.
-  array[*n - 1] = person;
-  min_heapify(array, *n - 1, *n);
-}
-
-void delete_from_min_heap(struct person *array, int *n) {
-  // Remove the minimum element from the heap.
-  array[0] = array[*n - 1];
-  (*n)--;
-
-  // Heapify the heap.
-  min_heapify(array, 0, *n);
-}
-
-void min_heapify(struct person *array, int i, int n) {
-  // Find the minimum child of the node.
-  int min_index = i;
-  int left_child = 2 * i + 1;
-  int right_child = 2 * i + 2;
-
-  if (left_child < n && array[left_child].age < array[min_index].age) {
-    min_index = left_child;
-  }
-
-  if (right_child < n && array[right_child].age < array[min_index].age) {
-    min_index = right_child;
-  }
-
-  // If the minimum child is smaller than the node, swap them.
-  if (min_index != i) {
-    struct person temp = array[i];
-    array[i] = array[min_index];
-    array[min_index] = temp;
-
-    min_heapify(array, min_index, n);
+    min_heapify(people, i, n);
   }
 }
 
 int main() {
-  // Initialize variables.
   int n;
-  struct person *array;
-
-  // Read the number of students.
   printf("Enter the number of students: ");
   scanf("%d", &n);
 
-  // Allocate memory for the array.
-array = (struct person *)malloc(n * sizeof(struct person));
-if (array == NULL) {
-  printf("Error allocating memory.\n");
-  exit(1);
-}
-   // Read the data from the file.
-  read_data(array, n);
+  struct person *people = malloc(sizeof(struct person) * n);
 
-  // Create the min-heap.
-  create_min_heap(array, n);
+  read_data(people, n);
 
-  // Print the weight of the youngest person.
-  display_weight_of_youngest_person(array, n);
+  build_min_heap(people, n);
 
-  // Menu driven program.
-  int option;
-  do {
+  int choice;
+  while (1) {
     printf("\nMAIN MENU (HEAP)\n");
     printf("1. Read Data\n");
     printf("2. Create a Min-heap based on the age\n");
@@ -133,42 +75,36 @@ if (array == NULL) {
     printf("6. Delete the oldest person\n");
     printf("7. Exit\n");
     printf("Enter option: ");
-    scanf("%d", &option);
+    scanf("%d", &choice);
 
-    switch (option) {
+    switch (choice) {
       case 1:
-        read_data(array, n);
+        read_data(people, n);
         break;
       case 2:
-        create_min_heap(array, n);
+        build_min_heap(people, n);
         break;
       case 3:
-        create_max_heap(array, n);
+        // Not implemented
         break;
       case 4:
-        display_weight_of_youngest_person(array, n);
+        printf("Weight of the youngest student: %d\n", people[0].weight);
         break;
-      case 5: {
-        struct person person;
-        printf("Enter the id, name, age, height and weight of the person: ");
-        scanf("%d %s %d %d %d", &person.id, person.name, &person.age, &person.height, &person.weight);
-        insert_into_min_heap(array, &n, person);
+      case 5:
+        // Not implemented
         break;
-      }
       case 6:
-        delete_from_min_heap(array, &n);
+        // Not implemented
         break;
       case 7:
-        printf("Exiting...\n");
-        break;
+        exit(0);
       default:
-        printf("Invalid option.\n");
+        printf("Invalid choice!\n");
         break;
     }
-  } while (option != 7);
+  }
 
-  // Free the memory.
-  free(array);
+  free(people);
 
   return 0;
 }
